2f2ad99c8dc5abafa044a55240214052c8f29166
hpscodemaverick <toli@Harishs-MBP.hsd1.ca.comcast.net>
2020-04-12 16:00:15 -0700

    MD updated

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
-//ES6 Sub Classes (Inheritance)
-class Person {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
+//#### Asynchronous Javascript, Ajax & Fetch
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
 
-  greeting() {
-    return `Hello ${this.firstName} ${this.lastName}`;
-  }
+function getJokes(e) {
+  //http://www.icndb.com/api/
+  //api: http://api.icndb.com/jokes/random/2
+
+  const number = document.querySelector('input[type="number').value;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+
+      let output = "";
+
+      if (response.type === "success") {
+        response.value.forEach(function (joke) {
+          output += `<li class='collection-item'>${joke.joke}</li>`;
+        });
+      } else {
+        output += `<li>Something went wrong</li>`;
+      }
+
+      document.querySelector(".jokes").innerHTML = output;
+
+      console.log(response);
+    }
+  };
+
+  xhr.send();
+  e.preventDefault();
 }
 
-// Customer inherit from Person
-class Customer extends Person {
-  constructor(firstName, lastName, phone, membership) {
-    super(firstName, lastName);
+/*
 
-    this.phone = phone;
-    this.membership = membership;
-  }
+{
+"type": "success",
+"value": [
+    {"id": 597,"joke": "Once death had a near Chuck Norris experience.","categories": []},
+    {"id": 554,"joke": "Chuck Norris can install a 64 bit OS on 32 bit machines.","categories": ["nerdy"]}
+  ]
 }
 
-const john = new Customer("John", "Doe", "552-444-7845", "Basic");
-console.log(john);
-console.log(john.greeting());
+*/
