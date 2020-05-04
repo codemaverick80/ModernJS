73da18ac47ab31be78e95592edaa4e21e90696d7
hpscodemaverick <toli@HarishsMacBookPro.hsd1.ca.comcast.net>
2020-05-04 10:29:43 -0700

    js module added

--- 6_higher_order_functions/reduce.js
+++ 6_higher_order_functions/reduce.js
@@ -15,15 +15,15 @@
  */
 
 const companies = [
-  { name: 'Company One', category: 'Finance', start: 1981, end: 2003 },
-  { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
-  { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
-  { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
-  { name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
-  { name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
-  { name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
-  { name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
-  { name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
+	{ name: 'Company One', category: 'Finance', start: 1981, end: 2003 },
+	{ name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+	{ name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+	{ name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+	{ name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+	{ name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
+	{ name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
+	{ name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
+	{ name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
 ];
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
@@ -50,3 +50,22 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 const ageSum = ages.reduce((total, age) => total + age, 0);
 
 console.log(ageSum);
+
+/* ======================= */
+const itemObj = [
+	{ price: 15, quantity: 2 },
+	{ price: 20, quantity: 3 },
+	{ price: 15, quantity: 4 },
+];
+
+// const itemCost=itemObj.reduce((total,item)=>{
+//   return total + item.price * item.quantity
+
+// },0);
+
+const itemCost = itemObj.reduce(
+	(total, item) => total + item.price * item.quantity,
+	0
+);
+
+console.log(`Item Total is ${itemCost}`);
